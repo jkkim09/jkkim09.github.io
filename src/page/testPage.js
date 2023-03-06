6978f245c45930f7f8078abaf70b1602eb51566f
jkkim09 <wkrud203@gmail.com>
2023-03-06 16:46:33 +0900

    마이프로필 react 개발

--- src/page/testPage.js
+++ src/page/testPage.js
@@ -1,5 +1,17 @@
+import '../styles/scss/myPage.scss'
+import MyImg from "../images/kakao.jpeg";
+
 const TestPage = () => {
-    return <div></div>
+    return <div className='myPage'>
+        <section className='myInfo'>
+            <div className='myInfo__profile'>
+                <div className='myInfo__profile__Image'>
+                    <img src={MyImg}/>
+                </div>
+            </div>
+        </section>
+        <section className='projectInfo'></section>
+    </div>
 }
 
 export default TestPage
